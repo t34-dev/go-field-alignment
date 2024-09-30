69ffa5dd2ee627b44f2390b4f9cf6e7d0713d2f0
T34 <t34.dev@gmail.com>
2024-09-30 18:36:45 +0300

    Upgrade tests

--- .make/test.mk
+++ .make/test.mk
@@ -1,10 +1,14 @@
+DEV_DIR := $(CURDIR)
+COVERAGE_FILE := $(DEV_DIR)/.temp/coverage-report.out
+HTML_COVERAGE := $(DEV_DIR)/.temp/coverage-report.html
+
 test:
 	@mkdir -p $(DEV_DIR)/.temp
-	@CGO_ENABLED=0 go test \
-	./cmd/gofield \
-	-coverprofile=$(DEV_DIR)/.temp/coverage-report.out -covermode=count
-	@go tool cover -html=$(DEV_DIR)/.temp/coverage-report.out -o $(DEV_DIR)/.temp/coverage-report.html
-	@go tool cover -func=$(DEV_DIR)/.temp/coverage-report.out
-
+	@go clean -testcache
+	@CGO_ENABLED=0 go test $(DEV_DIR)/cmd/gofield -coverprofile=coverage.tmp.out -covermode count -count 3
+	@grep -v 'mocks\|config\|main\.go' coverage.tmp.out  > $(COVERAGE_FILE)
+	@rm coverage.tmp.out
+	@go tool cover -html=$(COVERAGE_FILE) -o $(HTML_COVERAGE);
+	@go tool cover -func=$(COVERAGE_FILE) | grep "total";
 
 .PHONY: test
